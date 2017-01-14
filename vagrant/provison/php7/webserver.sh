c10e4c8adfee521e789f2ed3139e32f66b3ddbc3
itsmethemojo <marianpoeschmann@googlemail.com>
2017-01-14 23:13:49 +0100

    add vagrant provisioning scripts

---
+++ vagrant/provison/php7/webserver.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# cause ubuntu 16.04 does not work nice with vagrant, make sure php7 is available
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y \
+git \
+curl \
+nginx \
+php7.0 \
+php7.0-fpm \
+php7.0-xml \
+php7.0-zip \
+php7.0-curl
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+# replace nginx default config
+sudo rm /etc/nginx/sites-available/default
+sudo ln -s /vagrant/vagrant/default.conf /etc/nginx/sites-available/default
+
+# set up stadard project public folder as webroot
+sudo rm -rf /usr/share/nginx/html
+sudo ln -s /vagrant/public /usr/share/nginx/html
+
+sudo service nginx restart
